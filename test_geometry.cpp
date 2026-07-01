98c9c4c3f19731cf888db6315faa7bb970c02f04
ThibaultFillion <145604448+ThibaultFillion@users.noreply.github.com>
2026-07-01 23:00:25 +0200

    Update test_geometry.cpp
    - adding annoucement prints at the begining of tests
    - adding test for spherical to/from Cartesian units conversion

--- test_geometry.cpp
+++ test_geometry.cpp
@@ -19,11 +19,24 @@
 // SOFTWARE.
 // ==============================================================================
 
-#include "geometry.hpp"
 #include <cassert>
 #include <iostream>
+#include <vector>
+
+#include "geometry.hpp"
+#include "numerics.hpp"
+
+void assert_results(const std::vector<bool> & results){
+	for(size_t i=0; i<results.size(); i++){
+		std::string msg = results[i] ? "success" : "fail";
+		std::cout<<"\tcase "<<i<<": "<<msg<<std::endl;
+		assert(results[i]);
+		}
+	}
 
 void test_vec2(){
+	std::cout<<"test_vec2"<<std::endl;
+	
 	assert(Vec2(1, 2) + Vec2(2, 3) == Vec2(3, 5));
 	assert(Vec2(1, 2) - Vec2(3, 2) == Vec2(-2, 0));
 	assert(Vec2(1, 2) * 2 == Vec2(2, 4));
@@ -64,6 +77,8 @@ void test_vec2(){
 	}
 	
 void test_vec3(){
+	std::cout<<"test_vec3"<<std::endl;
+	
 	// ===========================
 	// operations and norm
 
@@ -131,6 +146,8 @@ void test_vec3(){
 	}
 
 void test_test_box_sphere_intersection(){
+	std::cout<<"test_test_box_sphere_intersection"<<std::endl;
+	
 	double r = 1.2;
 	Vec3 p0(1, 2, 3); // min corner
 	Vec3 dp(3.1, 7, 1); // extent (strictly positive)
@@ -147,14 +164,74 @@ void test_test_box_sphere_intersection(){
 	assert(test_box_sphere_intersection(p0, p1, c4, r) == false);
 	}
 
+void test_spherical_to_or_from_cartesian(){
+	std::cout<<"test_spherical_to_or_from_cartesian"<<std::endl;
+	
+	Vec3 v1(0, 1, 0);
+	Vec3 v2(0.5, 0, 0);
+	Vec3 v3(0, 0, 2);
+	Vec3 v4(0, 0, 0);
+	Vec3 v5(0, 0, -1);
+	Vec3 v6(-1, 0, 0);
+	Vec3 v7(-1, -1, 0);
+	
+	Vec3 v8(1.234, 3.42, 288);
+	Vec3 v9(-1.234, 3.42, 288);
+	Vec3 v10(1.234, -3.42, 288);
+	Vec3 v11(1.234, 3.42, -288);
+	Vec3 v12(-1.234, -3.42, 288);
+	Vec3 v13(1.234, -3.42, -288);
+	Vec3 v14(-1.234, 3.42, -288);
+	Vec3 v15(-1.234, 3.42, -288);
+
+	SphericalCoordinates c1 = cartesian_to_spherical(v1);
+	SphericalCoordinates c2 = cartesian_to_spherical(v2);
+	SphericalCoordinates c3 = cartesian_to_spherical(v3);
+	SphericalCoordinates c4 = cartesian_to_spherical(v4);
+	SphericalCoordinates c5 = cartesian_to_spherical(v5);
+	SphericalCoordinates c6 = cartesian_to_spherical(v6);
+	SphericalCoordinates c7 = cartesian_to_spherical(v7);
+	
+	std::vector<Vec3> all_v = {
+		v1, v2, v3, v4, v5, v6, v7, 
+	    v8, v9, v10, v11, v12, v13,
+		v14, v15
+		}; 
+		
+	bool validate_spherical_to_cartesian = true;
+	for(auto & v: all_v){
+		Vec3 v2 = spherical_to_cartesian(cartesian_to_spherical(v));
+		bool same_vec = 
+			are_abs_close(v.x, v2.x, 1e-10) and
+			are_abs_close(v.y, v2.y, 1e-10) and
+			are_abs_close(v.z, v2.z, 1e-10);
+		validate_spherical_to_cartesian &= same_vec;
+		}
+	
+	std::vector<bool> results {
+		are_rel_close(c1.r, 1) and are_rel_close(c1.phi, M_PI / 2) and are_rel_close(c1.theta, M_PI / 2),	
+		are_rel_close(c2.r, 0.5) and are_abs_close(c2.phi, 0, 1e-10) and are_rel_close(c2.theta, M_PI / 2), 
+		are_rel_close(c3.r, 2) and c3.phi == 0 and c3.theta == 0,
+		are_abs_close(c4.r, 0, 1e-10) and c4.phi == 0 and c4.theta == 0,
+		are_rel_close(c5.r, 1) and c5.phi == 0 and are_rel_close(c5.theta, M_PI),
+		are_rel_close(c6.r, 1) and are_rel_close(c6.phi, M_PI) and are_rel_close(c6.theta, M_PI / 2), 		
+		are_rel_close(c7.r, std::sqrt(2)) and 
+			(are_rel_close(c7.phi, 5 * M_PI / 4) or are_rel_close(c7.phi, -3 * M_PI / 4)) and 
+			are_rel_close(c7.theta, M_PI / 2),
+		validate_spherical_to_cartesian
+		};
+	
+	assert_results(results);
+	}
+
 void test(){
 	test_vec3();
 	test_vec2();
 	test_test_box_sphere_intersection();
+	test_spherical_to_or_from_cartesian();
 	}
 	
 int main(){
 	test();
 	return 0;
 	}
-
