d1c5c2aeece49fb75d08bd3645c3164fe740ab1f
ThibaultFillion <145604448+ThibaultFillion@users.noreply.github.com>
2026-07-18 15:54:21 +0200

    Update rng.hpp
    replacing std::discrete_distribution in draw with a direct inplementation for efficiency. Overload of draw for pre-computed weight sum

--- rng.hpp
+++ rng.hpp
@@ -62,7 +62,8 @@ class RandomNumberGenerator{
 					
 		double unit_rand(){
 		
-			// draws random double in [0, 1]
+			// draws random double in [0, 1),
+			// i.e 0 <= x~unit_rand < 1
 			
 			return unit_dist(rng);
 			}
@@ -79,8 +80,26 @@ class RandomNumberGenerator{
 			// draws an index (int) from 
 			// a discrete distribution
 			
-			std::discrete_distribution draw_dist(weights.begin(), weights.end());
-			return draw_dist(rng);
+			double total = std::accumulate(weights.begin(), weights.end(), 0.);
+			return draw(weights, total);
+			}
+			
+		int draw(const std::vector<double> & weights, double total){
+			
+			// draws an index (int) from 
+			// a discrete distribution,
+			// using the pre-computed
+			// weight sum
+			
+			double r = unit_rand()*total;
+			double cumul = 0;
+			for(size_t i = 0; i < weights.size(); i++){
+				cumul += weights[i];
+				if(cumul >= r){
+					return i;
+					}
+				}
+			return static_cast<int>(weights.size()-1);
 			}
 	};
 
