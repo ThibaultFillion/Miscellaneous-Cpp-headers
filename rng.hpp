56fe1a5e9e9216642b906dbc457efbd34b5b4ff7
ThibaultFillion <145604448+ThibaultFillion@users.noreply.github.com>
2026-07-18 14:46:37 +0200

    Create rng.hpp
    very simple header providing a modular rng class
    wrapping up the standard random header

---
+++ rng.hpp
@@ -0,0 +1,95 @@
+// ==============================================================================
+// MIT No Attribution
+// 
+// Copyright 2026 Thibault Fillion
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ==============================================================================
+
+// ==============================================================================
+// simple convenience wrapper around the
+// standard random header, to handle common 
+// random number generation in a compact
+// and modular way.
+//
+// classes:
+// - RandomNumberGenerator: class handling high-level RNG
+//
+// functions:
+// - generate_rand_seed_seq: generates a sequence of rng seeds
+// ==============================================================================
+
+#ifndef RNG_HPP
+#define RNG_HPP
+
+#include<random>
+#include<vector>
+
+class RandomNumberGenerator{
+	private:
+		std::mt19937 rng;
+		std::uniform_int_distribution<int> int_dist;
+		std::uniform_real_distribution<double> unit_dist;
+		
+	public:
+		RandomNumberGenerator(uint_fast32_t seed=0):
+			rng(seed),
+			unit_dist(0, 1){
+			}
+
+		int rand_int(){
+			
+			// draws random integer
+			
+			return int_dist(rng);
+			}
+					
+		double unit_rand(){
+		
+			// draws random double in [0, 1]
+			
+			return unit_dist(rng);
+			}
+		
+		bool btrial(double p){
+		
+			// Bernoulli trial on probability p
+			
+			return unit_rand() < p;
+			}
+		
+		int draw(const std::vector<double> & weights){
+			
+			// draws an index (int) from 
+			// a discrete distribution
+			
+			std::discrete_distribution draw_dist(weights.begin(), weights.end());
+			return draw_dist(rng);
+			}
+	};
+
+// generates a sequence of RNG seeds
+
+std::vector<uint_fast32_t> generate_rand_seed_seq(int seq_size, uint_fast32_t seed){
+	std::mt19937 rng(seed);
+	std::vector<uint_fast32_t> seed_seq(seq_size);
+	for(int i = 0; i < seq_size; i++){
+		seed_seq[i] = rng();
+		}	
+	return seed_seq;
+	}
+
+#endif // RNG_HPP
